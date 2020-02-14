11cbcfa6fcc59f02ca6b9f24281819fce8b37a54
Ganiiiii <ganeshharihar1211@gmail.com>
2020-02-14 18:49:05 +0530

    firebase integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'saragama';
+  title = "saragama";
+  constructor(private db: AngularFirestore) {}
+  ngOnInit() {
+    this.getUsers();
+  }
+  addUser() {
+    this.db.collection("users").add({ firstName: "amar", lastName: "harihar" });
+  }
+  getUsers() {
+    this.db
+      .collection("users")
+      .snapshotChanges()
+      .subscribe((users: any) => {
+        console.log("users:", users);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDHBhYaeGPQuVYpCodlXlhr3kkjjCsyHms",
+      authDomain: "saragama-31d9e.firebaseapp.com",
+      databaseURL: "https://saragama-31d9e.firebaseio.com",
+      projectId: "saragama-31d9e",
+      storageBucket: "saragama-31d9e.appspot.com",
+      messagingSenderId: "124296449440",
+      appId: "1:124296449440:web:e9648b3a0c14083ab5e8c5",
+      measurementId: "G-DCZ0ZRQNJY"
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
